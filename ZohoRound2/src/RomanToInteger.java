075142c14726f5b2e8ae88bdecaca86032d66e52
surendhar22102002 <loverboyffking143@gmail.com>
2026-01-02 18:30:02 +0530

    Roman to Integer

---
+++ ZohoRound2/src/RomanToInteger.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    static void main() {
+        System.out.println(romanToInt("LVIII"));
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int cur = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
+
+            if (cur < next) {
+                ans -= cur;
+            } else {
+                ans += cur;
+            }
+        }
+        return ans;
+    }
+}
